eb293a0c3c2483275a30316f57d3a572bcdd3c8a
Rick Meels <rick.meels@gmail.com>
2022-05-07 23:01:07 +0200

    Able to get current user posts #8

--- functions/get_current_user_post/app.py
+++ functions/get_current_user_post/app.py
@@ -1,13 +1,53 @@
+import os
+from datetime import datetime, timedelta
+
 import boto3
 import botocore.exceptions
 import json
 
+from aws_xray_sdk.core import patch_all
+from boto3.dynamodb.conditions import Key
+
+xray_patched = False
+
+dynamo_db = boto3.resource('dynamodb', 'eu-west-3')
+posts_table = dynamo_db.Table(os.environ.get('POSTS_TABLE'))
+
 
 def lambda_handler(event, context):
+    global xray_patched
+    if not xray_patched and 'DISABLE_XRAY' not in os.environ:
+        patch_all()
+        xray_patched = True
+
+    print(f'Received event: {event}')
+
+    user_id = event['requestContext']['authorizer']['claims']['sub']
+
+    response = posts_table.query(KeyConditionExpression=Key('PK').eq(user_id))
+
+    print(type(response))
+    print(response)
+
+    if response['Count'] == 0:
+        print(f'No posts found for user {user_id}')
+        return {
+            'statusCode': 204,
+            'body': json.dumps({'message': f'No posts found for user {user_id}'})
+        }
+    else:
+        print(f'Found {response["Count"]} posts for user {user_id}')
+        return {
+            'statusCode': 200,
+            'body': json.dumps(list(map(dynamo_item_to_post_model, response['Items'])))
+        }
+
+
+def dynamo_item_to_post_model(post_item):
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        'user_id': post_item['PK'],
+        'image_name': post_item['SK'],
+        'created_at': post_item['created_at'],
+        'caption': post_item['caption'],
+        'validated': post_item['validated']
     }
